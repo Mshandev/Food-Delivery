946e9d21c8866a8856b019c6143cd91e8d01f70e
Muhammad Shan <150339563+Mshandev@users.noreply.github.com>
2024-08-14 16:38:23 +0500

    Update Backend URL in App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/Login/Login";
 
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-5b6g.onrender.com";
   return (
     <div>
       <ToastContainer />
