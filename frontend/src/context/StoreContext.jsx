ada678c299fcefdeb6c10adb0509170a0ad48952
Muhammad Shan <150339563+Mshandev@users.noreply.github.com>
2024-08-14 16:46:37 +0500

    Update Backend Url in StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-5b6g.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
